0b674ef537dd1f8003692a8b3f08b10366671269
David Arias <davidarias.devs@gmail.com>
2025-01-02 19:03:46 -0500

    feat: eslint prettier and formatter config

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>My name is John Cena, tu tu turu tu</p>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "IndexPage",
+};
+</script>
