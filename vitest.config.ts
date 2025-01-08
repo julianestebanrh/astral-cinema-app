d71af68ed709ef626364af5e99709f5a0efbdd68
David Arias <davidarias.devs@gmail.com>
2025-01-08 18:17:11 -0500

    feat(Config): code quality scanning workflow (#2)
    * feat(Config): code quality scanning workflow
    
    * fix: change job runner to ubuntu-latest
    
    * feat: excluded files added to vitest coverage config
    
    * fix: sonar configuration file
    
    * fix: project key change
    
    * fix: checkout depth changed

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,25 @@
+import { configDefaults } from 'vitest/config';
 import { defineVitestConfig } from '@nuxt/test-utils/config';
+
+const filesToExclude = [
+  ...configDefaults.exclude,
+  '**/node_modules/**',
+  '**/.nuxt/**',
+  '**/types/**',
+  './nuxt.config.ts',
+  './tailwind.config.ts',
+]
 
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     setupFiles: ['./vitest.setup.ts'],
+    exclude: filesToExclude,
     coverage: {
       provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      all: true,
+      exclude: filesToExclude,
     },
     server: {
       deps: {
